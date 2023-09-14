af0c9909a04f7529e980cbb20a84a8713a14bf98
User <tunji4physics@gmail.com>
2023-09-14 13:16:49 +0100

    Intilizing structure when they are declared

--- part.cpp
+++ part.cpp
@@ -16,7 +16,15 @@ int main(){
   part1.partNumber = 373;
   part1.cost = 217.55F;
 
+  //You can initialize the structure variable when the structure is defined
+  part part2 = {5312, 551, 310.87};
+
+   cout<<"=============Part1 Variables==========================" <<endl;
   cout<< "Model: " << part1.modelnumber <<endl;
   cout<< "Part: "  << part1.partNumber <<endl;
   cout<< "Cost: " << part1.cost << endl;
+  cout<<"=============Part2 Variables==========================" <<endl;
+  cout<< "Model: " << part2.modelnumber <<endl;
+  cout<< "Part: "  << part2.partNumber <<endl;
+  cout<< "Cost: " << part2.cost << endl;
 }
